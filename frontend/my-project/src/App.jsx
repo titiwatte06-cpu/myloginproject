8282d9aaf0fc43236c9c8949a62000fe45d098dd
titiwatte06-cpu <titiwat.te06@gmail.com>
2026-05-17 19:29:00 +0700

    addedfront

--- frontend/my-project/src/App.jsx
+++ frontend/my-project/src/App.jsx
@@ -68,7 +68,7 @@ export default function App() {
   
   async function sendData() {
     try {
-        const res = await fetch('http://localhost:3000/login', {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
